9dcf0eb6d3f28332af59eb06fc08727d2f933a07
Sam Hu <samyhhu@github.com>
2022-09-17 16:04:14 -0700

    lower simdjson version

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class Recipe(ConanFile):
 
     def requirements(self):
         self.requires("fmt/8.1.1")
-        self.requires("simdjson/2.2.2")
+        self.requires("simdjson/1.0.2")
 
         # Testing only dependencies below
         self.requires("gtest/1.12.1")
